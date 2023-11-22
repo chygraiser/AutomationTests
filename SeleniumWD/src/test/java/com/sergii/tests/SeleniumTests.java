ae30fcd15004d3fb6ce0d97868206c8be2fcc12d
chygraiser <s.chygrai@gamil.com>
2023-11-22 18:30:24 +0100

    HomeWork 15 - start of configuration

---
+++ SeleniumWD/src/test/java/com/sergii/tests/SeleniumTests.java
@@ -0,0 +1,14 @@
+package com.sergii.tests;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class SeleniumTests {
+
+    private static final Logger log = LogManager.getLogger(SeleniumTests.class);
+
+    public static void main(String[] args) {
+        log.info("TestInfo");
+    }
+
+}
